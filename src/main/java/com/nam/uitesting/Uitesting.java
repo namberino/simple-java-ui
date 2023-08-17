ae2fa5f564130cb538eda9aed34b1ee58c95bb50
namber0 <namnb2402@gmail.com>
2023-08-17 08:49:02 +0700

    literally everything

---
+++ src/main/java/com/nam/uitesting/Uitesting.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.nam.uitesting;
+
+import java.awt.Component;
+import java.util.Vector;
+
+/**
+ *
+ * @author nam
+ */
+public class Uitesting 
+{
+    public static void main(String[] args)
+    {
+        form1 f = new form1();
+        f.setTitle("TEsting");
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+    }
+}
