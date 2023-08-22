670b3e88ea4811f7b50c55d692638d5e531ae17d
namber0 <namnb2402@gmail.com>
2023-08-22 09:21:54 +0700

    added console confirmation

--- src/main/java/com/nam/uitesting/Uitesting.java
+++ src/main/java/com/nam/uitesting/Uitesting.java
@@ -1,9 +1,14 @@
 package com.nam.uitesting;
 
+import java.util.Scanner;
+
 public class Uitesting 
 {
     public static void main(String[] args)
     {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        
         form1 f = new form1();
         f.setTitle("Testing");
         f.setLocationRelativeTo(null);
